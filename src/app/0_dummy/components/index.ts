ee2394ffa1e61365472d31eec46b4e44092ce857
mctrl <mctrl@users.noreply.github.com>
2019-11-26 19:22:35 +0000

    Updates

--- src/app/0_dummy/components/index.ts
+++
@@ -1,1 +0,0 @@
-export * from './basic-component/basic-component.component';
--- src/app/core/components/card/card.model.ts
+++
@@ -1,7 +0,0 @@
-export interface CardModel {
-    artName: string;
-    pageLink: string;
-    imageLink: string;
-    author: string;
-    githubLink: string;
-}
